7adf25d4e9644f0105676ef5a310b60a278bfef6
god-aryan27 <sp5686013@gmail.com>
2025-04-02 18:54:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from pyrogram.raw import functions, types
 from pyrogram import Client, idle
 from config import Config
 
-bot = Client
-    "bot",
+bot = Client,
     bot_token=Config.7722747376:AAFygs5miRtkjXLMzvaqVIFQJ9D2kcrcrxo,
     api_id=Config.23556070,
     api_hash=Config.83179ee32ab99592bf173238a72ca7f4,
     workers=50,
     plugins=dict(root="plugins")
-)
+
 bot.start()
 print("Bot Started ⚡")
 idle()
